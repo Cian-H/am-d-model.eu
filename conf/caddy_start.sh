5845bcaea557572edfee0303bb03ad429e3fa3e9
Cian Hughes <chughes000@gmail.com>
2025-01-16 11:41:14 +0000

    Added debug logs to caddy start script

--- conf/caddy_start.sh
+++ conf/caddy_start.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+set -x  # Enable debug output
 mkdir -p /etc/caddy
-curl https://raw.githubusercontent.com/Cian-H/am-d-model.eu/refs/heads/prod/conf/Caddyfile > /etc/caddy/Caddyfile
+curl -v https://raw.githubusercontent.com/Cian-H/am-d-model.eu/refs/heads/prod/conf/Caddyfile > /etc/caddy/Caddyfile
+ls -la /etc/caddy/Caddyfile  # Verify file exists and permissions
+cat /etc/caddy/Caddyfile     # Print contents to log
 exec caddy run --config /etc/caddy/Caddyfile
