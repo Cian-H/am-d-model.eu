450dbd3cc1b583b34c229e6627b38bce46304da0
Cian Hughes <chughes000@gmail.com>
2025-01-17 15:34:20 +0000

    Updated container definitions for deployment via docker compose

--- conf/caddy_start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -x  # Enable debug output
-mkdir -p /etc/caddy
-curl -v https://raw.githubusercontent.com/Cian-H/am-d-model.eu/refs/heads/prod/conf/Caddyfile > /etc/caddy/Caddyfile
-ls -la /etc/caddy/Caddyfile  # Verify file exists and permissions
-cat /etc/caddy/Caddyfile     # Print contents to log
-exec caddy run --config /etc/caddy/Caddyfile
