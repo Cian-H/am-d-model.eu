deb163ff38c080cd254c2a5ef03035fd42bc2f3b
Cian Hughes <chughes000@gmail.com>
2025-01-23 16:21:56 +0000

    Test of hook approach to solve sveltekit routing issues with reverse proxy

---
+++ src/hooks.server.js
@@ -0,0 +1,12 @@
+export const handle = async ({ event, resolve }) => {
+    // Paths to be handled by proxy
+    const proxyPaths = ["repo"];
+
+    if (proxyPaths.some(path => event.url.pathname.startsWith(path))) {
+        // Return empty response, letting proxy handle it
+        return new Response(null, { status: 404 });
+    }
+
+    // Let SvelteKit handle other paths
+    return resolve(event);
+};
