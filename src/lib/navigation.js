cd05e3fb640b9218dd03c188d14760c0d668c355
Cian Hughes <chughes000@gmail.com>
2025-01-24 11:28:26 +0000

    point navigation.js to server page

--- src/lib/navigation.js
+++ src/lib/navigation.js
@@ -1,11 +1,11 @@
 import { goto } from "$app/navigation";
-import { redirect, error } from '@sveltejs/kit';
+import { error } from '@sveltejs/kit';
 
 export const navigation = {
     toHome: () => goto("/"),
     toAbout: () => goto("/about"),
     toContact: () => goto("/contact"),
-    toRepository: () => redirect(307, "/repo"),
+    toRepository: () => goto("/repo"),
     to404: () => {
         throw error(404, 'Page not found');
     }
