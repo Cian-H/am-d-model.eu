7b4796d89c524212efb47e5566398bcea3f8cf88
Cian Hughes <chughes000@gmail.com>
2025-01-24 12:06:27 +0000

    Added winston debugger

---
+++ src/lib/server/logger.js
@@ -0,0 +1,15 @@
+import winston from 'winston';
+
+const { createLogger, transports, format } = winston;
+
+export const logger = createLogger({
+    level: 'debug',
+    format: format.combine(
+        format.timestamp(),
+        format.json()
+    ),
+    transports: [
+        new transports.File({ filename: 'logs/app.log' }),
+        new transports.Console()
+    ]
+});
