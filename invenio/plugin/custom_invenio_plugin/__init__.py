41aec686947a9420537a5e8f6c814b36509efe82
Cian Hughes <chughes000@gmail.com>
2025-05-08 13:05:04 +0100

    First working version of plugin

--- invenio/plugin/custom_invenio_plugin/__init__.py
+++ invenio/plugin/custom_invenio_plugin/__init__.py
@@ -5,7 +5,7 @@ from flask_menu import register_menu
 def init_app(app):
     """Initialize application."""
 
-    @app.route("/amdmodel_landing")
+    @app.route("/amdmodel")
     @register_menu(app, ".main.amdmodel", "About AM-D-Model")
     def redirect_to_amdmodel():
         """Redirect to the AM-D-Model website."""
--- invenio/plugin/setup.py
+++ invenio/plugin/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="custom_invenio_plugin",
-    version="0.1.0",
+    version="1.0.0",
     packages=["custom_invenio_plugin"],
     entry_points={
         "invenio_base.apps": [
