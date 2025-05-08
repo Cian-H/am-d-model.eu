e15cddbe2c246ac11537904437e156429791eb7e
Cian Hughes <chughes000@gmail.com>
2025-05-08 12:57:54 +0100

    Plugin debug

--- invenio/plugin/custom_invenio_plugin/__init__.py
+++ invenio/plugin/custom_invenio_plugin/__init__.py
@@ -1,22 +1,13 @@
 from flask import redirect
+from flask_menu import register_menu
 
 
 def init_app(app):
     """Initialize application."""
 
-    @app.route("/amdmodel_landing")
+    @register_menu(app, ".main.amdmodel", "About AM-D-Model")
     def redirect_to_amdmodel():
         """Redirect to the AM-D-Model website."""
         return redirect("https://am-d-model.eu")
-
-    @app.before_first_request
-    def init_main_menu():
-        from flask_menu import current_menu
-
-        current_menu.submenu("main").register(
-            "amdmodel_landing",
-            text="About AM-D-Model",
-            order=0,
-        )
 
     return app
