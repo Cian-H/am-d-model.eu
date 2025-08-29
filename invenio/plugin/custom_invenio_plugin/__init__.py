3b5036a444f95110bf9a780a3b0f3c7c825ac7b7
Cian Hughes <chughes000@gmail.com>
2025-08-29 11:25:19 +0100

    Anoter attempted plugin fix

--- invenio/plugin/custom_invenio_plugin/__init__.py
+++ invenio/plugin/custom_invenio_plugin/__init__.py
@@ -1,12 +1,11 @@
 from flask import redirect
-from flask_menu import current_menu
 
 
 def init_app(app):
     """Initialize application."""
 
     @app.route("/amdmodel")
-    @current_menu.register(".main.amdmodel", "About AM-D-Model")
+    @app.extensions["menu"].root().register(".main.amdmodel", "About AM-D-Model")
     def redirect_to_amdmodel():
         """Redirect to the AM-D-Model website."""
         return redirect("https://am-d-model.eu")
