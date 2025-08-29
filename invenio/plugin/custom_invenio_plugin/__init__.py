7c5143648029571c599779e65b03ad8c895bd3ef
Cian Hughes <chughes000@gmail.com>
2025-08-29 11:19:10 +0100

    Attempt to fix redirect plugin

--- invenio/plugin/custom_invenio_plugin/__init__.py
+++ invenio/plugin/custom_invenio_plugin/__init__.py
@@ -3,13 +3,12 @@ from flask_menu import current_menu
 
 
 def init_app(app):
-    """Initialize the application and register the menu item."""
+    """Initialize application."""
 
-    @app.route("/amdmodel", endpoint="main.amdmodel")
-    @current_menu.route("About AM-D-Model")
+    @app.route("/amdmodel")
+    @current_menu.register(".main.amdmodel", "About AM-D-Model")
     def redirect_to_amdmodel():
         """Redirect to the AM-D-Model website."""
         return redirect("https://am-d-model.eu")
 
     return app
-
