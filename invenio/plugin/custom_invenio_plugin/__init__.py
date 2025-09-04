9c2ff236a7f6f1f779bd6bc4c8f67215cb0e193c
Cian Hughes <chughes000@gmail.com>
2025-09-04 09:37:09 +0100

    Revert plugin to working state from invenio 12
    This reverts commit 640109d77eee2e9a48524b4bfb5c9520af50ca9c.

--- invenio/plugin/custom_invenio_plugin/__init__.py
+++ invenio/plugin/custom_invenio_plugin/__init__.py
@@ -1,24 +1,14 @@
 from flask import redirect
-from flask_menu import current_menu
+from flask_menu import register_menu
 
 
 def init_app(app):
     """Initialize application."""
 
     @app.route("/amdmodel")
+    @register_menu(app, ".main.amdmodel", "About AM-D-Model")
     def redirect_to_amdmodel():
         """Redirect to the AM-D-Model website."""
         return redirect("https://am-d-model.eu")
-
-    def register_menu_items():
-        with app.app_context():
-            menu_item = current_menu.submenu(".main.amdmodel")
-            menu_item.register(
-                endpoint='redirect_to_amdmodel',
-                text='About AM-D-Model'
-            )
-
-    with app.app_context():
-        register_menu_items()
 
     return app
